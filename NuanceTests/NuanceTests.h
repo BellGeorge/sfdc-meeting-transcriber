ff58cd305baf9ca325808be1c6b340a0e840de36
Elena <d.v.777@mail.ru>
2012-06-26 16:35:16 +0400

    Initial Commit

---
+++ NuanceTests/NuanceTests.h
@@ -0,0 +1,13 @@
+//
+//  NuanceTests.h
+//  NuanceTests
+//
+//  Created by Elena on 26.06.12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface NuanceTests : SenTestCase
+
+@end
---
+++ NuanceTests/NuanceTests.m
@@ -0,0 +1,32 @@
+//
+//  NuanceTests.m
+//  NuanceTests
+//
+//  Created by Elena on 26.06.12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "NuanceTests.h"
+
+@implementation NuanceTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in NuanceTests");
+}
+
+@end
